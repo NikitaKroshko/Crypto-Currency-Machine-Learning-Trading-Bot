789f83838bdc841f8ac49ab283750fb2a70e585d
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-04 12:07:52 +0100

    Create websocket function

--- data.py
+++ data.py
@@ -3,4 +3,8 @@ import websocket
 import pandas as pd
 
 def current_statistics(ticker):
-    
+    asset = ticker.lower() + "@kline_1m"
+
+def on_message(ws,message):
+    return json.loads(message)
+
