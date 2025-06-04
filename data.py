02f46f5a744c6f35f97387032f048fdf38061582
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-04 12:08:57 +0100

    Add the website for the socket

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ import json
 import websocket
 import pandas as pd
 
+socket = "wss://stream.binance.com:9443/stream?streams="
+
 def current_statistics(ticker):
     asset = ticker.lower() + "@kline_1m"
 
