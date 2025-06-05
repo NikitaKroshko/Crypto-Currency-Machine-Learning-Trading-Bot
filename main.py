17b4314ab677537f172fcd03f4c55a5e8163d1bd
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-05 12:14:56 +0100

    Change names

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import data
 import time
 
-def save_data(tickers,filename, time = 1):
+def save_data(tickers,filename, duration = 1):
     with open(filename, "w") as file:
         t_end = time.time() + 60 * time
         while(time.time() < t_end):
@@ -12,7 +12,7 @@ def save_data(tickers,filename, time = 1):
 tickers = input("Which tickers do you want to track (split by ,): ")
 input_list = tickers.split(",")
 filename = input("Name of the file you want the tickers to be stored into: ")
-time = int(input("Enter the length of time you want the tickers to be tracker for: "))
-save_data(input_list, filename, time)
+duration = int(input("Enter the length of time you want the tickers to be tracker for: "))
+save_data(input_list, filename, duration )
 
 
