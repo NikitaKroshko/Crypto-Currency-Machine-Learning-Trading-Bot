7b92b96bc0275193513c3ce8a75cdf034d91b24f
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-05 12:13:14 +0100

    Start writing the data to a file, and requesting data from the user via the terminal

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import data
+import time
 
-def save_data(tickers,filename):
-    with
+def save_data(tickers,filename, time = 1):
+    with open(filename, "w") as file:
+        t_end = time.time() + 60 * time
+        while(time.time() < t_end):
+            parallel_current_statistics(tickers)
+        for line in data.data:
+            file.write(line + "\n")
+
+tickers = input("Which tickers do you want to track (split by ,): ")
+input_list = tickers.split(",")
+filename = input("Name of the file you want the tickers to be stored into: ")
+
+
