3dd54ebfda44ebe43a03e5a4d97799bb500368f6
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-05 12:23:56 +0100

    Save files now work everything works as intended but the timed stopping which is unfortunate

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import time
 
 def save_data(tickers,filename, duration = 1):
     with open(filename, "w") as file:
-        t_end = time.time() + 60 * time
+        t_end = time.time() + 60 * duration 
         while(time.time() < t_end):
-            parallel_current_statistics(tickers)
-        for line in data.data:
-            file.write(line + "\n")
+            data.parallel_current_statistics(tickers)
+        file.writelines(str(data.data))
 
 tickers = input("Which tickers do you want to track (split by ,): ")
 input_list = tickers.split(",")
 filename = input("Name of the file you want the tickers to be stored into: ")
 duration = int(input("Enter the length of time you want the tickers to be tracker for: "))
-save_data(input_list, filename, duration )
+save_data(input_list, filename, duration)
 
 
