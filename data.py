4851f17aa960d30f7340c3210254326355e914b3
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-05 12:02:49 +0100

    Add comments

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ socket = "wss://stream.binance.com:9443/stream?streams="
 data = []
 
 def current_statistics(ticker):
+    #Takes a ticker symbol for crypto_currencies and populates a data list of the file with that ticker symbols data
     asset = ticker.lower() + "@kline_1m"
     socket_msg = socket+asset
     ws = websocket.WebSocketApp(socket_msg, on_message=on_message)
@@ -17,6 +18,7 @@ def on_message(ws,message):
     print(len(data))
 
 def parallel_current_statistics(tickers):
+    # Same principle as for the current statistics but this takes a list of tickers and returns data for all of them
     assets = []
     for asset in tickers:
         assets.append(asset.lower() + "@kline_1m")
