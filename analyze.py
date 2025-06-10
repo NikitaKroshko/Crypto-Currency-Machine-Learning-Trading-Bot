31b15bc3d9ab8812817a2aaa37b77ddebe10c38a
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-10 21:12:46 +0100

    Read the data into a json obj, then make the df from the json

--- analyze.py
+++ analyze.py
@@ -1,10 +1,12 @@
-import nummpy as np
+import numpy as np
 import pandas as pd
 import json
 
-df = Null
+df = None
 
 def populate_df():
     df_data = data.data
-    
-
+    json_data = json.dumps(df_data)
+    global df
+    df = pd.read_json(json_data)
+    return df
