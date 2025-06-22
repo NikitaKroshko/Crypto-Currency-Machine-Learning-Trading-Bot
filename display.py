e3fbc94d3bcd6f8fc7893e7b59a937f255850b7b
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-06-22 20:02:12 +0200

    display the mean of the column and ticker based on in[uts to the function

--- display.py
+++ display.py
@@ -9,3 +9,6 @@ def display_data():
 def display_analyzed_data():
     # Basic print out of the df of the analyzed data
     print(analyze.df)
+
+def display_mean(column, ticker):
+    print(analyze.mean(column, ticker))
